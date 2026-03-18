c796e7f4b16b9512634f940ad2a67aa5c6f7d57f
UjjwalBBhatta <ujjwal.117320@mitnepal.edu.np>
2026-03-18 14:45:43 +0545

    Create a corrleation heatmap between AAPl, MSFT, and GOOG

--- analysis/multi_asset.py
+++ analysis/multi_asset.py
@@ -2,5 +2,25 @@ import numpy as np
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-from utils.stats import compute_corelation, compute_covariance, compute_variance
+import seaborn as sns
 
+"""1. Data fetching"""
+
+#Lets get the data for Apple, Google, and Microsoft
+tickers = ["AAPL", "MSFT", "GOOG"]
+data = yf.download(tickers, start = "2022-01-01", end = "2025-01-01")
+#Clean the data
+print(data.head())
+
+#Get the closing prices
+close_prices = data["Close"]
+
+#Get the returns
+returns = close_prices.pct_change()
+
+#Correlation matrix
+corr_matrix = returns.corr()
+
+sns.heatmap(corr_matrix, annot=True, cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.show()
